3819b9d0bc3682c76df94d5d421953774bbb671b
danieldor1 <127633810+danieldor1@users.noreply.github.com>
2024-05-28 23:58:37 -0400

    Code Work: Robust error handling added to this module.

--- cloud-functions/func_trascriptora/send_transcription.py
+++ cloud-functions/func_trascriptora/send_transcription.py
@@ -1,11 +1,16 @@
 import requests
 import json
 
+from retry import retry
+from http import HTTPStatus
+
+
+@retry(tries=3, delay=1)
 def send_transcription_to_cloud_function(
     transcript: str, 
     user_id: str,
     conversation_id: str,
-    url: str) -> None:
+    url: str) -> HTTPStatus:
     
     data = {
         'transcript': transcript, 
@@ -16,7 +21,24 @@ def send_transcription_to_cloud_function(
     headers = {"Content-Type": "application/json"}
     
     try:
-        response = requests.post(url, data=json.dumps(data), headers=headers)
+        response = requests.post(url, data=json.dumps(data), headers=headers, timeout=5)
         response.raise_for_status()
-    except requests.RequestException as e:
-        raise TranscriptionError(f"Error sending transcript to Cloud Function: {e}") from e
+        
+        return HTTPStatus.OK
+    
+    except requests.exceptions.ConnectionError:
+        return HTTPStatus.SERVICE_UNAVAILABLE
+
+    except requests.exceptions.Timeout:
+        return HTTPStatus.REQUEST_TIMEOUT
+    
+    except requests.exceptions.HTTPError:
+        return HTTPStatus.BAD_REQUEST
+    
+    except requests.exceptions.RequestException:
+        return HTTPStatus.INTERNAL_SERVER_ERROR
+    
+
+
+
+
