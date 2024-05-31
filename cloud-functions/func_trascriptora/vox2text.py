29b34a066ec3db9dc07000ef1a98ebe4bcbc7cee
danieldor1 <127633810+danieldor1@users.noreply.github.com>
2024-05-30 23:43:50 -0400

    Code Work: Added a custom exception and a class to work with the constants of the code

--- cloud-functions/func_trascriptora/vox2text.py
+++ cloud-functions/func_trascriptora/vox2text.py
@@ -1,13 +1,18 @@
-import io
 import azure.cognitiveservices.speech as speech2text
-
 from pydub import AudioSegment
-from speech_recognition import Recognizer, AudioData
+from constants import InvocatorKeyManager, ConfigConstans
+from exceptions import TranscriptionError
+
+
+AZURE_SPEECH2TEXT_API_KEY = InvocatorKeyManager.azure_api_key.value
+AZURE_SPEECH2TEXT_REGION = ConfigConstans.azure_service_region.value
+RECOGNITION_LANGUAGE = ConfigConstans.azure_recognition_language.value
 
 speech_config = speech2text.SpeechConfig(
     subscription=AZURE_SPEECH2TEXT_API_KEY, 
     region=AZURE_SPEECH2TEXT_REGION
 )
+
 speech_config.speech_recognition_language= RECOGNITION_LANGUAGE
 
 
@@ -16,7 +21,7 @@ def transcribe_audio(audio_data: bytes) -> str:
     audio_clip = AudioSegment.from_file(audio_data)
     
     try:
-        audio_config = speech2text.audio.AudioConfig(filename=tmp_path)
+        audio_config = speech2text.audio.AudioConfig(filename=audio_clip)
         speech_recognizer = speech2text.SpeechRecognizer(
             speech_config=speech_config, 
             audio_config=audio_config
@@ -26,4 +31,4 @@ def transcribe_audio(audio_data: bytes) -> str:
         return str(result_of_transcription_in_text.strip())  
     
     except Exception:
-        []
+        raise TranscriptionError
